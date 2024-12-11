05e135274992de69a4a7ad325068d7a64907f55e
sangnv <sangnv19093@gmail.com>
2024-12-12 02:06:19 +0700

    update

--- firebase/index.js
+++ firebase/index.js
@@ -2,7 +2,7 @@ const admin = require("firebase-admin");
 
 // Initialize firebase admin SDK
 
-const serviceAccount = require("../beefoodconsole-firebase-adminsdk-xv6qz-7a3da32a33.json");
+const serviceAccount = require("../beefoodconsole-firebase-adminsdk-xv6qz-eafb0fdb92.json");
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   storageBucket: "beefoodconsole.appspot.com",
